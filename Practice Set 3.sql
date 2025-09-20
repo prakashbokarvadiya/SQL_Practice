2a93abfc124b28b101061526df090ad9eeb3475c
thakorhim <117174109+thakorhim@users.noreply.github.com>
2025-09-20 22:28:02 +0530

    SQL practice

--- Practice Set 3.sql
+++ Practice Set 3.sql
@@ -27,3 +27,12 @@ ALTER TABLE emp
 CHANGE COLUMN hire_date join_date date;
 
 
+
+SELECT customer_id,
+       COUNT(order_id) AS total_orders,
+       MAX(order_date) AS latest_order_date
+FROM orders
+GROUP BY customer_id;
+
+
+
