f588523a47f26f2ceed92ef8c9c5f09813a4e76d
thakorhim <117174109+thakorhim@users.noreply.github.com>
2025-09-20 22:27:01 +0530

    SQL practice

---
+++ Practice Set 3.sql
@@ -0,0 +1,29 @@
+use abc;
+show tables;
+select * from department ;
+select * from emp;
+drop database usb;
+use usb;
+drop table emp;
+show tables;
+drop table student;
+show databases;
+
+select d_id,sum(salary)
+from emp
+group by d_id
+having count(e_id) > 2;
+
+SELECT DISTINCT salary
+FROM emp
+ORDER BY salary DESC
+LIMIT 1 OFFSET 2;
+
+SELECT *
+FROM emp
+WHERE YEAR(hire_date) = 2021;
+
+ALTER TABLE emp 
+CHANGE COLUMN hire_date join_date date;
+
+
